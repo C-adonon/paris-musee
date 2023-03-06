a4190064e09f9b7493d9b56f2f0a82b565a6af42
Chloé <cadonon.dev@gmail.com>
2023-03-06 14:01:29 +0100

    imported paintings route

---
+++ server/routes/paintings.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { fetchAPI } from "../service/fetchAPI.js";
+import { getPainting } from "../controler/queries.js";
+const router = express.Router();
+
+// 
+// GET A SPECIFIED PAINTING ROUTE
+// 
+
+router.get("/:id", async (req, res, next) => {
+  console.log(process.env.API_AUTH_TOKEN);
+  let paintings;
+  try {
+    paintings = await fetchAPI(
+      getPainting(req.params.id),
+      process.env.API_AUTH_TOKEN
+    );
+    res.json(paintings);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+export default router;
