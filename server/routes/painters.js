024b1a450db9d2f7a297387d7618cdc54810c136
Chloé <cadonon.dev@gmail.com>
2023-03-08 00:21:07 +0100

    created painters route

---
+++ server/routes/painters.js
@@ -0,0 +1,88 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import { AllPainters } from "../controler/getAllPainters.js";
+import { getOnePainter } from "../controler/getOnePainter.js";
+import { getPaintersPaintings } from "../controler/getPaintersPaintings.js";
+import { GetRandomPainters } from "../controler/getRandomPainters.js";
+import { getOneRandomPainting } from "../controler/getOneRandomPainting.js";
+
+const prisma = new PrismaClient();
+const router = express.Router();
+
+//
+// GETS ALL PAINTERS
+//
+
+router.get("/", async (req, res, next) => {
+  try {
+    let painters = await AllPainters(prisma);
+    res.json(painters);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+//
+// GETS A SPECIFIED PAINTER
+//
+
+router.get("/:id", async (req, res, next) => {
+  const painterId = req.params.uuid;
+  try {
+    let painter = await getOnePainter(prisma, painterId);
+    res.json(painter);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+//
+// GETS ALL PAINTINGS OF A SPECIFIED PAINTER
+//
+
+router.get("/:id/paintings", async (req, res, next) => {
+  const painterId = parseInt(req.params.id);
+  try {
+    let paintings = await getPaintersPaintings(prisma, painterId);
+    res.json(paintings);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+//
+// GETS A RANDOM SET OF FOUR PAINTERS
+//
+
+router.get("/random", async (req, res, next) => {
+  try {
+    // let x = Math.floor(Math.random() * 7);
+    let randomPainters = await GetRandomPainters(prisma);
+    res.json(randomPainters);
+    // res.json("random painters");
+  } catch (error) {
+    return next(error);
+  }
+});
+
+//
+// GETS A RANDOM PAINTING FROM A SPECIFIED PAINTER
+//
+
+router.get("/:id/random", async (req, res, next) => {
+  const painterId = parseInt(req.params.id);
+  try {
+    let AllPaintings = await getPaintersPaintings(prisma, painterId);
+    let AllPaintingsLength = AllPaintings.length;
+    let randomPainting = await getOneRandomPainting(
+      prisma,
+      painterId,
+      AllPaintingsLength
+    );
+    res.json(randomPainting);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+export default router;
