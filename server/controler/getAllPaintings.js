6605b8bcc110318e1e940d7eba7a82a760382ae2
Chloé <cadonon.dev@gmail.com>
2023-03-08 00:19:07 +0100

    clean up

--- server/controler/getAllPaintings.js
+++ server/controler/getAllPaintings.js
@@ -1,8 +1,5 @@
-import express from "express";
 import { PrismaClient } from "@prisma/client";
-import createHttpError from "http-errors";
 
-const router = express.Router();
 const prisma = new PrismaClient();
 
 export async function AllPaintings() {
