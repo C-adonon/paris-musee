5e595e58b03cdf974003cbe239e6aef0a9c1f680
Chloé <cadonon.dev@gmail.com>
2023-03-08 00:20:29 +0100

    added function to send painter's data to DB

--- server/routes/paintings.js
+++ server/routes/paintings.js
@@ -1,9 +1,12 @@
 import express from "express";
-import createHttpError from "http-errors";
+import { PrismaClient } from "@prisma/client";
 import { fetchAPI } from "../service/fetchAPI.js";
 import { getPainting } from "../service/queries.js";
 import { sendPaintingData } from "../controler/insertPaintingData.js";
+import { sendPainterData } from "../controler/insertPainterData.js";
 import { AllPaintings } from "../controler/getAllPaintings.js";
+
+const prisma = new PrismaClient();
 const router = express.Router();
 
 //
@@ -21,6 +24,8 @@ router.get("/:uuid", async (req, res, next) => {
       url: painting[0].fieldVisuels[0].entity.vignette,
     };
     let painterData = {
+      name: painting[0].fieldOeuvreAuteurs[0].entity.fieldAuteurAuteur.entity
+        .name,
       birthDate:
         painting[0].fieldOeuvreAuteurs[0].entity.fieldAuteurAuteur.entity
           .fieldPipDateNaissance.sort,
@@ -28,8 +33,11 @@ router.get("/:uuid", async (req, res, next) => {
         painting[0].fieldOeuvreAuteurs[0].entity.fieldAuteurAuteur.entity
           .fieldPipDateDeces.sort,
     };
-    let paintingResult = await sendPaintingData(paintingData, uuid);
-    res.json(paintingResult);
+    let paintingResult = await sendPaintingData(prisma, paintingData, uuid);
+    let painterId = paintingResult.painter_id;
+    console.log(painterId);
+    let painterResult = await sendPainterData(prisma, painterData, painterId);
+    res.json({ paintingResult, painterResult });
     // res.json(painting);
   } catch (error) {
     return next(error);
@@ -42,7 +50,7 @@ router.get("/:uuid", async (req, res, next) => {
 
 router.get("/", async (req, res, next) => {
   try {
-    let paintings = await AllPaintings();
+    let paintings = await AllPaintings(prisma);
     res.json(paintings);
   } catch (error) {
     return next(error);
