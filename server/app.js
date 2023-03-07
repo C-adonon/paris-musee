38d1f164a25070fc8ac9ba2cfe610203ef24e5e9
Chloé <cadonon.dev@gmail.com>
2023-03-07 15:10:32 +0100

    modified error middleware for undefined err.status

--- server/app.js
+++ server/app.js
@@ -12,7 +12,8 @@ app.use(cors({}));
 
 // For parsing application/json
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 
 // Router
 app.use("/admin", admin);
@@ -21,12 +22,15 @@ app.use("/paintings", paintings);
 // Middleware d'erreurs
 app.use((err, req, res, next) => {
   // Erreurs auth
-  //   if (err.name === "UnauthorizedError") {
-  //     return res.status(401).json({ msg: "Vous n'avez pas accès à cette page" });
-  //   }
+  if (err.name === "UnauthorizedError") {
+    return res.status(401).json({ msg: "Vous n'avez pas accès à cette page" });
+  }
   if (err.name === "ErrorDocument") {
     return res.status(404).json({ msg: "Cette page n'existe pas" });
   }
   // autres erreurs à gérer
+  if (err.status == undefined) {
+    return res.status(500).json({ error: err.message });
+  }
   return res.status(err.status).json({ error: err.message });
 });
