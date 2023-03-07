6e93018dc61e5a794b7cac9105b095b5521df51b
Chloé <cadonon.dev@gmail.com>
2023-03-08 00:16:09 +0100

    clean up

--- server/controler/insertPaintingData.js
+++ server/controler/insertPaintingData.js
@@ -1,11 +1,4 @@
-import express from "express";
-import { PrismaClient } from "@prisma/client";
-import createHttpError from "http-errors";
-
-const router = express.Router();
-const prisma = new PrismaClient();
-
-export async function sendPaintingData(paintingData, uuid) {
+export async function sendPaintingData(prisma, paintingData, uuid) {
   const painting = await prisma.paintings.update({
     where: {
       painting_uuid: uuid,
