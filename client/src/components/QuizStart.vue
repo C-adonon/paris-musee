32b328d4e3567d107d1f294cae0639f9d1b77a26
Chloé <cadonon.dev@gmail.com>
2023-03-09 00:50:35 +0100

    created gameStart component

---
+++ client/src/components/QuizStart.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="quiz-start">
+        <h1>Quiz</h1>
+        <button @click="startQuiz">Démarrer le Quiz</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'QuizStart',
+    methods: {
+        startQuiz() {
+            this.$router.push('/quiz');
+        }
+    }
+}
+</script>
