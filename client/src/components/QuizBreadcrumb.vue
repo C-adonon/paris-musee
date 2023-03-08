4ef9c6eef2af68c22b87609177cff97209320682
Chloé <cadonon.dev@gmail.com>
2023-03-08 23:13:54 +0100

    initialized component

--- client/src/components/QuizBreadcrumb.vue
+++ client/src/components/QuizBreadcrumb.vue
@@ -1,3 +1,27 @@
 <template>
-    <p>QuizBreadcrumb</p>
+    <h3>Quiz ({{ questionNbr }}/10)</h3>
 </template>
+
+<script>
+export default {
+    name: 'QuizBreadcrumb',
+    props: {
+        questionNbr: {
+            type: Number,
+            required: true
+        }
+    },
+}
+</script>
+
+<style scoped lang="scss">
+@import "../assets/main.scss";
+
+h3 {
+    border-bottom: 1px solid $light-blue;
+    width: fit-content;
+    padding: 10px;
+    margin: auto;
+    font-weight: 400;
+}
+</style>
