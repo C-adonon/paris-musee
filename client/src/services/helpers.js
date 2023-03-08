1a5d5f438881c1d2fa1583b06ed24f483105be0a
Chloé <cadonon.dev@gmail.com>
2023-03-08 14:57:18 +0100

    bug fix on urls

--- client/src/services/helpers.js
+++ client/src/services/helpers.js
@@ -3,11 +3,11 @@ import axios from "axios";
 const API = "http://localhost:3000";
 
 export async function getRandomPainters() {
-  const response = await axios.get(`${API}/random`);
+  const response = await axios.get(`${API}/painters/random`);
   return response.data;
 }
 
 export async function getPaintingById(id) {
-  const response = await axios.get(`${API}/painting/${id}`);
+  const response = await axios.get(`${API}/paintings/${id}`);
   return response.data;
 }
