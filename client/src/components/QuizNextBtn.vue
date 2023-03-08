9f3d366ce0a018e824a830be9337d14b222575e9
Chloé <cadonon.dev@gmail.com>
2023-03-08 23:06:24 +0100

    added methods & style

--- client/src/components/QuizNextBtn.vue
+++ client/src/components/QuizNextBtn.vue
@@ -1,3 +1,34 @@
 <template>
-    <div><img src="../assets/icon/arrow_forward_FILL0_wght400_GRAD0_opsz48.svg" alt="next icon"></div>
+    <div class="next" @click="nextQuestion()"><img src="../assets/icon/arrow_forward_FILL0_wght400_GRAD0_opsz48.svg"
+            alt="next icon">
+    </div>
 </template>
+
+<script>
+export default {
+    name: 'QuizNextBtn',
+    emits: ['nextQuestion'],
+    data() {
+        return {
+            nextBtn: true
+        }
+    },
+    methods: {
+        nextQuestion() {
+            this.$emit('nextQuestion', this.nextBtn)
+        }
+    }
+}
+
+</script>
+
+<style scoped lang="scss">
+@import "../assets/main.scss";
+
+div.next {
+    background-color: $light-blue;
+    width: 50px;
+    height: 50px;
+
+}
+</style>
