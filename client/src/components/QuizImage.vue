73512afe9bbf8f7e20007d70157b6d9051d03774
Chloé <cadonon.dev@gmail.com>
2023-03-08 23:14:18 +0100

    Initialized component

--- client/src/components/QuizImage.vue
+++ client/src/components/QuizImage.vue
@@ -1,3 +1,25 @@
 <template>
-    <img src="#" alt="Quiz Image" />
+    <img :src="this.imageUrl" alt="Quiz Image" />
 </template>
+
+<script>
+export default {
+    name: 'QuizImage',
+    props: {
+        imageUrl: {
+            type: String,
+            required: true
+        }
+    },
+}
+
+</script>
+
+<style scoped lang="scss">
+@import "../assets/main.scss";
+
+img {
+    width: 100%;
+    object-fit: cover;
+}
+</style>
