d2c4a36800954d47254f5d1442bcb237e3002e3f
Chloé <cadonon.dev@gmail.com>
2023-03-08 23:15:49 +0100

    added helper to get a random painting w/ painterId

--- client/src/services/helpers.js
+++ client/src/services/helpers.js
@@ -2,11 +2,19 @@ import axios from "axios";
 
 const API = "http://localhost:3000";
 
+// Gets 4 random painters
 export async function getRandomPainters() {
   const response = await axios.get(`${API}/painters/random`);
   return response.data;
 }
 
+// Gets a random painting from a specified painter
+export async function getRandomPaintingByPainterId(id) {
+  const response = await axios.get(`${API}/painters/${id}/random`);
+  return response.data;
+}
+
+// Gets a specified painting
 export async function getPaintingById(id) {
   const response = await axios.get(`${API}/paintings/${id}`);
   return response.data;
