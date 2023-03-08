84fbed519353a6a39d68bb31001717032cd56a5d
Chloé <cadonon.dev@gmail.com>
2023-03-09 00:44:24 +0100

    created endQuiz component

---
+++ client/src/components/EndQuiz.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { useStore } from '@/stores/stores.js';
+</script>
+
+<template>
+    <div>
+        <h1>This is the end of the quiz</h1>
+        <h2>Your score is {{ score }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'EndQuiz',
+    data() {
+        return {
+            score: 0
+        }
+    },
+    created() {
+        this.score = useStore().getScore;
+    }
+};
+
+</script>
