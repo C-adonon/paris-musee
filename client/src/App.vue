bf5369ef11464bb4e78646d7c756994886687284
Chloé <cadonon.dev@gmail.com>
2023-03-08 23:01:25 +0100

    clean up

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,13 +3,6 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <!-- <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav> -->
-
-  </header>
   <RouterView />
 </template>
 
