4279698c8aef92787267b00b7232b13981343aab
Chloé <cadonon.dev@gmail.com>
2023-03-09 01:14:33 +0100

    clean up & modified routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,29 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import GameEnd from '../views/GameEnd.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Game from "../views/Game.vue";
+import GameEnd from "../views/GameEnd.vue";
+import GameStart from "../views/GameStart.vue";
+import Admin from "../views/Admin.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/game",
+      name: "game",
+      component: Game,
     },
     {
-      path: '/game-end',
-      name: 'game-end',
-      component: GameEnd
-    }
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
-  ]
-})
+      path: "/game-end",
+      name: "game-end",
+      component: GameEnd,
+    },
+    {
+      path: "/",
+      name: "game-start",
+      component: GameStart,
+    },
+    {
+      path: "/admin",
+      name: "admin",
+      component: Admin,
+    },
+  ],
+});
 
-export default router
+export default router;
