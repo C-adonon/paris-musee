6420b2df6c53915fdc861377a6135bd22d71dd58
Chloé <cadonon.dev@gmail.com>
2023-03-15 19:47:53 +0100

    added controller in random painting route & modified imports

--- server/routes/painters.js
+++ server/routes/painters.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
-import { AllPainters } from "../controler/getAllPainters.js";
-import { getOnePainter } from "../controler/getOnePainter.js";
-import { getPaintersPaintings } from "../controler/getPaintersPaintings.js";
-import { GetRandomPainters } from "../controler/getRandomPainters.js";
-import { getOneRandomPainting } from "../controler/getOneRandomPainting.js";
+import { AllPainters } from "../service/DB/getAllPainters.js";
+import { getOnePainter } from "../service/DB/getOnePainter.js";
+import { getPaintersPaintings } from "../service/DB/getPaintersPaintings.js";
+import { GetRandomPainters } from "../service/DB/getRandomPainters.js";
+import { getOneRandomPainting } from "../service/DB/getOneRandomPainting.js";
+import { getPaintingController } from "../controller/paintingsController.js";
 
 const prisma = new PrismaClient();
 const router = express.Router();
@@ -70,13 +71,11 @@ router.get("/random", async (req, res, next) => {
 router.get("/:id/random", async (req, res, next) => {
   const painterId = parseInt(req.params.id);
   try {
-    let AllPaintings = await getPaintersPaintings(prisma, painterId);
-    let AllPaintingsLength = AllPaintings.length;
-    let randomPainting = await getOneRandomPainting(
-      prisma,
-      painterId,
-      AllPaintingsLength
-    );
+    // Controller test
+    let randomPainting = await getPaintingController(painterId);
+    if (!randomPainting) {
+      return next(createHttpError(404, "No painting found"));
+    }
     res.json(randomPainting);
   } catch (error) {
     return next(error);
