0a3fda7c40d00ce7a5ce725b00c5efa6b5ff7c3a
Chloé <cadonon.dev@gmail.com>
2023-03-18 00:22:22 +0100

    added env variables config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import dotenv from "dotenv";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// Load environment variables
+dotenv.config();
+
+// Get the environment variables
+const { VUE_APP_API_URL } = process.env;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  define: {
+    // Pass the environment variables to the app
+    "process.env": {
+      VUE_APP_API_URL: JSON.stringify(VUE_APP_API_URL),
+    },
+  },
+});
