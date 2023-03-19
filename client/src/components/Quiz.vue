773857e5108be50838bc79cb33952bc7666e46c8
Chloé <cadonon.dev@gmail.com>
2023-03-20 00:48:45 +0100

    clean up

--- client/src/components/Quiz.vue
+++ client/src/components/Quiz.vue
@@ -82,7 +82,6 @@ export default {
     updateScore() {
       this.score++;
       useStore().setScore(this.score);
-      // console.log(useStore().getScore);
     },
 
     // Initialize the quiz
@@ -99,7 +98,6 @@ export default {
       this.updateAllGameAnswers();
       // sets the image url
       this.imageUrl = this.currentPainting.url;
-      // console.log(this.currentPainting.url);
     },
 
     // Updates AllGameAnswers
@@ -116,34 +114,22 @@ export default {
     // Removes duplicates
     async removeDuplicates(paintinUuid) {
       let previousAnswers = this.getAllGameAnswers();
-      // console.log("Previous Answers :");
-      // console.table(previousAnswers);
       let isDuplicate = previousAnswers.includes(paintinUuid);
+      // Polling
       while (isDuplicate) {
-        // console.log("isDuplicate :" + isDuplicate);
         let newPainting = await getRandomPaintingByPainterId(this.currentPainter.id);
         if (newPainting.painting_uuid !== this.currentPainting.painting_uuid && previousAnswers.includes(newPainting.painting_uuid) == false) {
-          // if (previousAnswers.includes(newPainting.painting_uuid) == false) {
-          // console.log("Current Painting :");
-          // console.log(this.currentPainting);
           this.currentPainting = newPainting;
-          // console.log("New Painting :");
-          // console.log(this.currentPainting);
-          // console.log(newPainting);
           isDuplicate = false;
           break;
         } else {
           // All paintings from the current painter have already been answered
           // Creates a new array with all the painters except the current painter
           let newPainterTab = this.painters.filter(painter => painter.id !== this.currentPainter.id);
-          // console.log(newPainterTab);
           // Gets a random painter from the new array
           this.currentPainter = newPainterTab[Math.floor(Math.random() * newPainterTab.length)];
           // Gets a random painting from the new painter
           this.currentPainting = await getRandomPaintingByPainterId(this.currentPainter.id);
-          // console.log(this.currentPainting);
-          // Assigns the new painting to the current painting
-          // this.currentPainting = newCurrentPainting;
         }
       }
     },
@@ -151,8 +137,7 @@ export default {
     // Updates the round
     updateRound() {
       this.round++;
-      // useStore().setRound(this.round);
-      // console.log(useStore().getRound);
+      useStore().setRound(this.round);
     },
 
     // Reset the quiz
@@ -168,13 +153,10 @@ export default {
     // Goes to the next question or ends the quiz
     nextQuestion() {
       this.updateRound();
-      // this.updateAllGameAnswers();
       if (this.round > this.totalRounds) {
         this.isQuizFinished = true;
         router.replace({ path: '/game-end' })
       } else {
-        // updates AllGameAnswers
-        // console.table(this.getAllGameAnswers());
         this.resetQuestion();
         this.initializeQuiz();
       }
